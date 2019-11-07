af63c745db08fd9c7679c50ab18d224b5e1b8719
Jota <mfjotaa@hotmail.com>
2019-11-07 16:29:43 +0100

    final

--- pbeurre/settings.py
+++ pbeurre/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import dj_database_url
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -161,5 +160,3 @@ if os.environ.get('ENV') == 'PRODUCTION':
     db_from_env = dj_database_url.config(conn_max_age=500)
 
     DATABASES['default'].update(db_from_env)
-
-django_heroku.settings(locals())
