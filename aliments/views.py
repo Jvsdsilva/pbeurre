08a4b7385f94131ecf4c748d6cded9d7975f874a
unknown <mfjotaa@hotmail.com>
2019-11-09 11:07:18 +0100

    finals

--- aliments/views.py
+++ aliments/views.py
@@ -60,7 +60,7 @@ def signup(request):
 def results(request):
     dbInsert.insertCategory()
     dbInsert.insertStore()
-    """result_res = []
+    result_res = []
     search = request.POST.get('searchbtn', None)
     query_index = request.POST['query_index']
     query_nav = request.POST['query_nav']
@@ -92,7 +92,7 @@ def results(request):
             result_res.append(contexts)
         # print(result_res)
         return render(request, 'aliments/results.html',
-                      {'results': result_res})"""
+                      {'results': result_res})
 
 
 # redirect to page details for a specific product
